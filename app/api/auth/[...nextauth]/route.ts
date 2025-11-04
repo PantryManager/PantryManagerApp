5575711338b57ce565adbc83f7df68295d89dfa7
Anton Sakhanovych <dev@antonsakhanovych.com>
2025-11-04 14:16:25 -0500

    get nextauth secret from env

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -6,6 +6,7 @@ import GithubProvider from 'next-auth/providers/github'
 
 export const authOptions: NextAuthOptions = {
     adapter: PrismaAdapter(prisma),
+    secret: process.env.NEXTAUTH_SECRET,
     providers: [
         GithubProvider({
             clientId: process.env.GITHUB_ID!,
